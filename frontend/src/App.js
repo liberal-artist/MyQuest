61e1d35128efb5ff72e8cb67a76c974c66e0276c
JunYoung <luks@JunYoungui-MacBookAir.local>
2024-08-04 22:28:13 +0900

    [frontend] Hello World

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function Header() {
+  return (
+    <>
+      MyQuest
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
